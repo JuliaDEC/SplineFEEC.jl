449136bee106394e7674bede7de68f95996faf5b
Documenter.jl <documenter@juliadocs.github.io>
2020-11-05 14:03:49 +0000

    build based on 6ed4e8f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SplineFEEC","category":"page"},{"location":"#SplineFEEC","page":"Home","title":"SplineFEEC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SplineFEEC]","category":"page"}]
+}
